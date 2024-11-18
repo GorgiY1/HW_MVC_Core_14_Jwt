738fdfab718748f47cf6f84842480a2b804cc35a
johnny <orgeorgij@gmail.com>
2024-11-18 16:50:16 +0200

    Correct initial

--- HW_MVC_Core_14_Jwt/HW_MVC_Core_14_Jwt/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HW_MVC_Core_14_Jwt.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
