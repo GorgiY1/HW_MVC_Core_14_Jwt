eff810e520538925b0bd86ac6f7801eaba6b55e1
johnny <orgeorgij@gmail.com>
2024-11-18 16:48:05 +0200

    Add project files.

---
+++ HW_MVC_Core_14_Jwt/HW_MVC_Core_14_Jwt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HW_MVC_Core_14_Jwt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
